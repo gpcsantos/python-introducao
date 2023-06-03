b769e332292753045e24f62a98b1cf5978ff324f
gpcsantos <gpcsantos@gmail.com>
2023-06-02 22:02:18 -0300

    O Jogo - adivinhe qual o número - foi finalizado

--- cap02_Atividade02.py
+++ cap02_Atividade02.py
@@ -1,4 +1,5 @@
 from os import system
+from random import randint
 system('cls') # limpa a tela
 
 print('***************************************')
@@ -7,14 +8,26 @@ print('***************************************')
 
 
 # Relembrar constantes e variávies
-numero_secreto = 83 #constante
+numero_secreto = randint(0,100)
 
-rodada = 0
-total_de_tentativas = 3
+pontos = 1000
 acerto = False
 
-while (rodada <= total_de_tentativas):
-    rodada += 1
+print('Qual o nível de dificuldade?')
+print('(1) Padawan \n(2) Cavaleiro \n(3) Mestre Jedi ')
+nivel = int(input('\nDefina o nível: '))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+pontos_a_perder = int(pontos / total_de_tentativas)
+print(f'Sua pontuaçãoa atual: {pontos}')
+
+for rodada in range (1, total_de_tentativas+1):
     tentativa = input('Tente acertar o número: ')
     print('Você digitou: ', tentativa)
     print(f'\nTentativa {rodada:02d} de {total_de_tentativas:02d}')
@@ -27,13 +40,18 @@ while (rodada <= total_de_tentativas):
         break
     except Exception as e:
         print(f'ERRO: {e}')
-        break
+        break # interrrompe o laço
     else:
-        pass
+        pass #pass a diante
         # print('\nentrou no ELSE')
     finally:
         pass
         # print('\nentrou no FINALLY')
+
+    if (tentativa_int < 1 or tentativa_int > 100):
+        print('Tentativa inválida! Somente números de 1 a 100')
+        rodada -= 1
+        continue # volta no início do laço, ignorando o que está abaixo
 
     # segundo o guia de estilo para Python, é sempre, que possível, necessário melhorar 
     # a legibilidade do código.
@@ -45,15 +63,25 @@ while (rodada <= total_de_tentativas):
 
     # Nova estrututa de IFs
     if acerto:
-        print('Boa tentativa. ACERTOU!!!!')
+        print(f'Boa tentativa. ACERTOU!!!! \nFez {pontos}!')
         #exit()
         break
     else:
+        pontos_proximidade = 50 - abs(numero_secreto - tentativa_int)
+        pontos = pontos - pontos_a_perder + pontos_proximidade
         print('Não foi dessa vez. ERROU!!!!')
         if ehmaior:
             print('Sua tentativa foi MAIOR que o número secreto.')
         if ehmenor:
             print('Sua tentativa foi MENOR que o número secreto.')
+        if (rodada < total_de_tentativas):
+            print(f'Sua pontuação atual: {pontos}')
+        else:
+            print('\n-----------------------------')
+            print(f'O númeor secreto era {numero_secreto}')
+            print(f'Sua pontuação final: {pontos}')
+            print('-----------------------------')
+            
         
 if not acerto:
     print('GAME OVER!')
--- cap03_Atividade01.py
+++ cap03_Atividade01.py
@@ -2,11 +2,11 @@ import random
 
 # Diferença entre int e round... INT retira a parte decimal "trunca"...   ROUD arredonda
 numero_secreto = int(93.90659198739872293)
-print(numero_secreto)
+print(numero_secreto) # 93
 
 # Diferença entre int e round... INT retira a parte decimal "trunca"...   ROUD arredonda
 numero_secreto = round(93.90659198739872293)
-print(numero_secreto)
+print(numero_secreto) # 94
 
 # random gera um número decial entre 0 e 1, por isso multiplicamos por 100
 numero_secreto = int(random.random() * 100)
@@ -14,3 +14,11 @@ print(numero_secreto)
 
 numero_secreto = random.randrange(0,101)
 print(numero_secreto)
+
+numero_secreto = random.randint(0,11)
+print(numero_secreto)
+
+numero_secreto = random.randrange(1,10, 2)
+print(numero_secreto)
+
+
